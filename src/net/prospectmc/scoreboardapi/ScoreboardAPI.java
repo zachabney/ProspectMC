e6e6e83d8b588c84409817153b0ab7cca415f2e9
Cody Sommer <codisimus@gmail.com>
2013-12-06 13:23:20 -0500

    Made attribute parameter optional for '/att value'

---
+++ src/net/prospectmc/scoreboardapi/ScoreboardAPI.java
@@ -0,0 +1,10 @@
+package net.prospectmc.scoreboardapi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ScoreboardAPI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+}
