272e8eebe1c3ee6a0aaaca41f1f4d973170637ab
Cody Sommer <codisimus@gmail.com>
2013-12-06 13:34:31 -0500

    Create scoreboard when a player logs in

--- src/net/prospectmc/scoreboardapi/ScoreboardAPI.java
+++ src/net/prospectmc/scoreboardapi/ScoreboardAPI.java
@@ -1,10 +1,23 @@
 package net.prospectmc.scoreboardapi;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class ScoreboardAPI extends JavaPlugin {
+public class ScoreboardAPI extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        createScoreboard(event.getPlayer());
+    }
+
+    private static void createScoreboard(Player player) {
+        player.setScoreboard(Bukkit.getScoreboardManager().getNewScoreboard());
     }
 }
