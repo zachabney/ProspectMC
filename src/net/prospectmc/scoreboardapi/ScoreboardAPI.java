3165b4e00c70083747bb9aa3c72cf0dafbaed913
Cody Sommer <codisimus@gmail.com>
2013-12-06 15:39:12 -0500

    create teams based on Factions

--- src/net/prospectmc/scoreboardapi/ScoreboardAPI.java
+++ src/net/prospectmc/scoreboardapi/ScoreboardAPI.java
@@ -1,13 +1,13 @@
 package net.prospectmc.scoreboardapi;
 
+import net.prospectmc.Faction;
+import net.prospectmc.FactionAPI;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scoreboard.DisplaySlot;
-import org.bukkit.scoreboard.Objective;
 import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.scoreboard.Team;
 
@@ -20,21 +20,21 @@ public class ScoreboardAPI extends JavaPlugin implements Listener {
 
 	@EventHandler
 	public void onPlayerJoin(PlayerJoinEvent event) {
-		Scoreboard board = Bukkit.getScoreboardManager().getMainScoreboard();
-		Team team1 = board.getTeam("team1");
-		if(team1 == null) team1 = board.registerNewTeam("team1");
-		Team team2 = board.getTeam("team2");
-		if(team2 == null) team2 = board.registerNewTeam("team2");
-		team1.setPrefix("§a");
-		team2.setPrefix("§c");
+                Player player = event.getPlayer();
+		Scoreboard board = Bukkit.getScoreboardManager().getNewScoreboard();
+                player.setScoreboard(board);
 
-		if(team1.getSize() > 0) {
-			team2.addPlayer(event.getPlayer());
-		} else {
-                        team1.addPlayer(event.getPlayer());
+                for (Faction faction : Faction.values()) {
+                        Team t = board.registerNewTeam(faction.toString());
+                        t.setPrefix(faction.getColor().toString());
+                        for (String playerName : faction.getMembers()) {
+                                t.addPlayer(Bukkit.getOfflinePlayer(playerName));
+                        }
                 }
-                event.getPlayer().sendMessage("You have been added to team " + (team1.getPlayers().contains(event.getPlayer()) ? 1 : 2));
-		event.getPlayer().setScoreboard(board);
-	}
 
+		Team team = board.getTeam(FactionAPI.findFaction(player.getName()).toString());
+                for (Player p : Bukkit.getOnlinePlayers()) {
+                        p.getScoreboard().getTeam(team.getName()).addPlayer(player);
+                }
+	}
 }
