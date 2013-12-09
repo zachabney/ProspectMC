c68e481d0c6184d50263394cdcfdd6b3429093de
Zach Abney <zachabney@me.com>
2013-12-08 21:52:50 -0600

    Fixing codys stupid formatting

--- src/net/prospectmc/damageapi/DamageAPI.java
+++ src/net/prospectmc/damageapi/DamageAPI.java
@@ -20,123 +20,135 @@ import org.bukkit.scheduler.BukkitTask;
 
 /**
  * DamageAPI is used to apply damage and status effects for weapons/items
- *
+ * 
  * @author Codisimus
  */
 public class DamageAPI extends JavaPlugin implements Listener {
-    private static JavaPlugin plugin;
-    private static HashMap<UUID, DamageSnapshot> projectiles = new HashMap<UUID, DamageSnapshot>();
-    private static HashMap<String, LivingEntity> attributeEffects = new HashMap<String, LivingEntity>();
-    private static HashMap<String, BukkitTask> tasks = new HashMap<String, BukkitTask>();
+	private static JavaPlugin plugin;
+	private static HashMap<UUID, DamageSnapshot> projectiles = new HashMap<UUID, DamageSnapshot>();
+	private static HashMap<String, LivingEntity> attributeEffects = new HashMap<String, LivingEntity>();
+	private static HashMap<String, BukkitTask> tasks = new HashMap<String, BukkitTask>();
 
-    @Override
-    public void onEnable() {
-        plugin = this;
-        Bukkit.getPluginManager().registerEvents(this, this);
-    }
+	@Override
+	public void onEnable() {
+		plugin = this;
+		Bukkit.getPluginManager().registerEvents(this, this);
+	}
 
-    /**
-     * Registers an entity as being damaged by status effects
-     *
-     * @param victim The LivingEntity being damaged
-     * @param cause The DamageCause that the ProspectDamageEvent will detect
-     * @param attacker The LivingEntity which caused the damage
-     * @param duration How long the damage is to be registered for
-     */
-    public static void registerDamageCause(LivingEntity victim, DamageCause cause, LivingEntity attacker, int duration) {
-        //Create a unique key
-        final String key = victim.getUniqueId() + "." + cause.name();
-        //New status effects overwrite old ones
-        if (tasks.containsKey(key)) {
-            tasks.get(key).cancel();
-        }
-        attributeEffects.put(key, attacker);
-        //Trigger to remove the key in the set duration
-        tasks.put(key, new BukkitRunnable() {
-            @Override
-            public void run() {
-                attributeEffects.remove(key);
-                tasks.remove(key);
-            }
-        }.runTaskLater(plugin, duration));
-    }
+	/**
+	 * Registers an entity as being damaged by status effects
+	 * 
+	 * @param victim
+	 *            The LivingEntity being damaged
+	 * @param cause
+	 *            The DamageCause that the ProspectDamageEvent will detect
+	 * @param attacker
+	 *            The LivingEntity which caused the damage
+	 * @param duration
+	 *            How long the damage is to be registered for
+	 */
+	public static void registerDamageCause(LivingEntity victim,
+			DamageCause cause, LivingEntity attacker, int duration) {
+		// Create a unique key
+		final String key = victim.getUniqueId() + "." + cause.name();
+		// New status effects overwrite old ones
+		if (tasks.containsKey(key)) {
+			tasks.get(key).cancel();
+		}
+		attributeEffects.put(key, attacker);
+		// Trigger to remove the key in the set duration
+		tasks.put(key, new BukkitRunnable() {
+			@Override
+			public void run() {
+				attributeEffects.remove(key);
+				tasks.remove(key);
+			}
+		}.runTaskLater(plugin, duration));
+	}
 
-    /**
-     * Returns the Entity which is causing the damage cause if any
-     *
-     * @param victim The LivingEntity being damaged
-     * @param cause The DamageCause
-     * @return The LivingEntity which is register as the damage causer, or null if none are registered
-     */
-    public static LivingEntity getAttacker(LivingEntity victim, DamageCause cause) {
-        String key = victim.getUniqueId() + "." + cause.name();
-        return attributeEffects.get(key);
-    }
+	/**
+	 * Returns the Entity which is causing the damage cause if any
+	 * 
+	 * @param victim
+	 *            The LivingEntity being damaged
+	 * @param cause
+	 *            The DamageCause
+	 * @return The LivingEntity which is register as the damage causer, or null
+	 *         if none are registered
+	 */
+	public static LivingEntity getAttacker(LivingEntity victim,
+			DamageCause cause) {
+		String key = victim.getUniqueId() + "." + cause.name();
+		return attributeEffects.get(key);
+	}
 
-    /**
-     * Registers the given Entity as a projectile
-     *
-     * @param shooter The LivingEntity responsible for launching the projectile
-     * @param thrown The Entity which is being used as a projectile
-     */
-    public static void registerProjectile(LivingEntity shooter, org.bukkit.entity.Entity thrown) {
-        projectiles.put(thrown.getUniqueId(), new DamageSnapshot(shooter));
-    }
+	/**
+	 * Registers the given Entity as a projectile
+	 * 
+	 * @param shooter
+	 *            The LivingEntity responsible for launching the projectile
+	 * @param thrown
+	 *            The Entity which is being used as a projectile
+	 */
+	public static void registerProjectile(LivingEntity shooter,
+			org.bukkit.entity.Entity thrown) {
+		projectiles.put(thrown.getUniqueId(), new DamageSnapshot(shooter));
+	}
 
-    /**
-     * Returns the DamageSnapshot for the given Entity
-     * If the Entity is a Player, a new snapshot will be created
-     * otherwise the registered snapshot will be returned if there is one
-     *
-     * @param damager The Entity which is causing damage
-     * @return The DamageSnapshot for the Entity
-     */
-    public static DamageSnapshot getDamageSnapshot(Entity damager) {
-        return damager instanceof Player
-               ? new DamageSnapshot((Player) damager)
-               : projectiles.remove(damager.getUniqueId());
-    }
+	/**
+	 * Returns the DamageSnapshot for the given Entity If the Entity is a
+	 * Player, a new snapshot will be created otherwise the registered snapshot
+	 * will be returned if there is one
+	 * 
+	 * @param damager
+	 *            The Entity which is causing damage
+	 * @return The DamageSnapshot for the Entity
+	 */
+	public static DamageSnapshot getDamageSnapshot(Entity damager) {
+		return damager instanceof Player ? new DamageSnapshot((Player) damager)
+				: projectiles.remove(damager.getUniqueId());
+	}
 
-    @EventHandler (ignoreCancelled = true, priority = EventPriority.MONITOR)
-    public void onProjectileLaunch(ProjectileLaunchEvent event) {
-        final Projectile projectile = event.getEntity();
-        LivingEntity shooter = projectile.getShooter();
-        if (shooter == null) {
-            return;
-        }
-        DamageAPI.registerProjectile(shooter, projectile);
-        //Free up space after time
-        new BukkitRunnable() {
-            @Override
-            public void run() {
-                projectiles.remove(projectile.getUniqueId());
-            }
-        }.runTaskLater(this, 20 * 30);
-    }
+	@EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
+	public void onProjectileLaunch(ProjectileLaunchEvent event) {
+		final Projectile projectile = event.getEntity();
+		LivingEntity shooter = projectile.getShooter();
+		if (shooter == null) {
+			return;
+		}
+		DamageAPI.registerProjectile(shooter, projectile);
+		// Free up space after time
+		new BukkitRunnable() {
+			@Override
+			public void run() {
+				projectiles.remove(projectile.getUniqueId());
+			}
+		}.runTaskLater(this, 20 * 30);
+	}
 
-    @EventHandler (ignoreCancelled = true, priority = EventPriority.HIGHEST)
-    public void onEntityDamage(EntityDamageEvent event) {
-        //We only care about hurting living things
-        if (!(event.getEntity() instanceof LivingEntity)) {
-            return;
-        }
+	@EventHandler(ignoreCancelled = true, priority = EventPriority.HIGHEST)
+	public void onEntityDamage(EntityDamageEvent event) {
+		// We only care about hurting living things
+		if (!(event.getEntity() instanceof LivingEntity)) {
+			return;
+		}
 
-        LivingEntity victim = (LivingEntity) event.getEntity();
-        Entity attacker = event instanceof EntityDamageByEntityEvent
-                          ? ((EntityDamageByEntityEvent) event).getDamager()
-                          : null;
-        double damage = event.getDamage();
-        DamageCause cause = event.getCause();
+		LivingEntity victim = (LivingEntity) event.getEntity();
+		Entity attacker = event instanceof EntityDamageByEntityEvent ? ((EntityDamageByEntityEvent) event)
+				.getDamager() : null;
+		double damage = event.getDamage();
+		DamageCause cause = event.getCause();
 
-        //Trigger a ProspectDamageEvent
-        ProspectDamageEvent pEvent = new ProspectDamageEvent(victim, attacker, damage, cause);
-        if (pEvent.isCancelled()) {
-            event.setCancelled(true);
-        } else {
-            event.setDamage(pEvent.getDamage());
-            if (pEvent.hasSnapshot()) {
-                pEvent.getDamageSnapshot().applyDamage(victim);
-            }
-        }
-    }
+		// Trigger a ProspectDamageEvent
+		ProspectDamageEvent pEvent = new ProspectDamageEvent(victim, attacker,
+				damage, cause);
+		if (pEvent.isCancelled()) {
+			event.setCancelled(true);
+		} else {
+			event.setDamage(pEvent.getDamage());
+			if (pEvent.hasSnapshot()) {
+				pEvent.getDamageSnapshot().applyDamage(victim);
+			}
+		}
+	}
 }
--- src/net/prospectmc/damageapi/DamageSnapshot.java
+++ src/net/prospectmc/damageapi/DamageSnapshot.java
@@ -13,143 +13,161 @@ import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
 /**
- * A DamageSnapshot holds data of a specific LivingEntity at the time of attacking
- *
+ * A DamageSnapshot holds data of a specific LivingEntity at the time of
+ * attacking
+ * 
  * @author Codisimus
  */
 public class DamageSnapshot {
-    private static final ArrayList<Attribute> ATTRIBUTES = new ArrayList<Attribute>();
-    private LivingEntity entity; //The LivingEntity causing damage
-    private ItemStack weapon; //The weapon they used to cause the damage
-    private EnumMap<Attribute, Double> attributeValues = new EnumMap<Attribute, Double>(Attribute.class); //A snapshot of relevent attribute values at the time
-    private double weaponEfficiency; //A snapshot of their efficiency at the time
+	private static final ArrayList<Attribute> ATTRIBUTES = new ArrayList<Attribute>();
+	private LivingEntity entity; // The LivingEntity causing damage
+	private ItemStack weapon; // The weapon they used to cause the damage
+	private EnumMap<Attribute, Double> attributeValues = new EnumMap<Attribute, Double>(
+			Attribute.class); // A snapshot of relevent attribute values at the
+								// time
+	private double weaponEfficiency; // A snapshot of their efficiency at the
+										// time
 
-    /**
-     * Add custom attributes that have status effects locally
-     */
-    static {
-        for (Attribute attribute : Attribute.values()) {
-            if (attribute.hasPotionEffect()) {
-                ATTRIBUTES.add(attribute);
-            }
-        }
-        ATTRIBUTES.add(Attribute.FIRE_DAMAGE);
-    }
+	/**
+	 * Add custom attributes that have status effects locally
+	 */
+	static {
+		for (Attribute attribute : Attribute.values()) {
+			if (attribute.hasPotionEffect()) {
+				ATTRIBUTES.add(attribute);
+			}
+		}
+		ATTRIBUTES.add(Attribute.FIRE_DAMAGE);
+	}
 
-    /**
-     * Constructs a new DamageSnapshot for the given entity
-     *
-     * @param entity The given LivingEntity
-     */
-    public DamageSnapshot(LivingEntity entity) {
-        this.entity = entity;
-        weapon = entity.getEquipment().getItemInHand();
-        for (Attribute attribute : ATTRIBUTES) {
-            double value = AttributeAPI.getValue(entity, attribute);
-            //Only add attribute values that will matter
-            if (value > 0) {
-                attributeValues.put(attribute, value);
-            }
-        }
-        weaponEfficiency = AttributeAPI.getValue(entity, Attribute.WEAPON_EFFECIENCY);
-    }
+	/**
+	 * Constructs a new DamageSnapshot for the given entity
+	 * 
+	 * @param entity
+	 *            The given LivingEntity
+	 */
+	public DamageSnapshot(LivingEntity entity) {
+		this.entity = entity;
+		weapon = entity.getEquipment().getItemInHand();
+		for (Attribute attribute : ATTRIBUTES) {
+			double value = AttributeAPI.getValue(entity, attribute);
+			// Only add attribute values that will matter
+			if (value > 0) {
+				attributeValues.put(attribute, value);
+			}
+		}
+		weaponEfficiency = AttributeAPI.getValue(entity,
+				Attribute.WEAPON_EFFECIENCY);
+	}
 
-    /**
-     * Returns the Entity that this snapshot is for
-     *
-     * @return The LivingEntity that is represented
-     */
-    public LivingEntity getEntity() {
-        return entity;
-    }
+	/**
+	 * Returns the Entity that this snapshot is for
+	 * 
+	 * @return The LivingEntity that is represented
+	 */
+	public LivingEntity getEntity() {
+		return entity;
+	}
 
-    /**
-     * Returns the weapon that was used
-     *
-     * @return The ItemStack which was used to cause damage
-     */
-    public ItemStack getWeapon() {
-        return weapon;
-    }
+	/**
+	 * Returns the weapon that was used
+	 * 
+	 * @return The ItemStack which was used to cause damage
+	 */
+	public ItemStack getWeapon() {
+		return weapon;
+	}
 
-    //Too expensive?
-    //public EnumSet<Attribute> possibleAttributeEffects(LivingEntity entity) {
-    //    EnumSet<Attribute> attributes = EnumSet.noneOf(Attribute.class);
-    //    for (Attribute attribute : attributeValues.keySet()) {
-    //        double damage = attributeValues.get(attribute);
-    //        if (attribute.hasResistance()) {
-    //            damage -= AttributeAPI.getValue(entity, attribute.getResistance());
-    //        }
-    //        if (damage > 0) {
-    //            attributes.add(attribute);
-    //        }
-    //    }
-    //    return attributes;
-    //}
+	// Too expensive?
+	// public EnumSet<Attribute> possibleAttributeEffects(LivingEntity entity) {
+	// EnumSet<Attribute> attributes = EnumSet.noneOf(Attribute.class);
+	// for (Attribute attribute : attributeValues.keySet()) {
+	// double damage = attributeValues.get(attribute);
+	// if (attribute.hasResistance()) {
+	// damage -= AttributeAPI.getValue(entity, attribute.getResistance());
+	// }
+	// if (damage > 0) {
+	// attributes.add(attribute);
+	// }
+	// }
+	// return attributes;
+	// }
 
-    /**
-     * Applies damage/status effects to the given Entity
-     *
-     * @param entity The LivingEntity to be damaged
-     */
-    void applyDamage(LivingEntity entity) {
-        double armorEffeciency = AttributeAPI.getValue(entity, Attribute.WEAPON_EFFECIENCY);
+	/**
+	 * Applies damage/status effects to the given Entity
+	 * 
+	 * @param entity
+	 *            The LivingEntity to be damaged
+	 */
+	void applyDamage(LivingEntity entity) {
+		double armorEffeciency = AttributeAPI.getValue(entity,
+				Attribute.WEAPON_EFFECIENCY);
 
-        //Apply Ice Damage
-        double damage;
-        if (attributeValues.containsKey(Attribute.ICE_DAMAGE)) {
-            damage = (attributeValues.get(Attribute.ICE_DAMAGE) - AttributeAPI.getValue(entity, Attribute.ICE_RESISTANCE)) / 5;
-            if (damage > 0) {
-                DamageAPI.registerDamageCause(this.entity, DamageCause.MELTING, entity, 1);
-                EntityDamageByEntityEvent event = new EntityDamageByEntityEvent(this.entity, entity, EntityDamageByEntityEvent.DamageCause.MELTING, damage);
-                Bukkit.getPluginManager().callEvent(event);
-                if (!event.isCancelled()) {
-                    double health = entity.getHealth() - event.getDamage();
-                    if (health < 0) {
-                        health = 0;
-                    }
-                    entity.setHealth(health);
-                }
-            }
-        }
+		// Apply Ice Damage
+		double damage;
+		if (attributeValues.containsKey(Attribute.ICE_DAMAGE)) {
+			damage = (attributeValues.get(Attribute.ICE_DAMAGE) - AttributeAPI
+					.getValue(entity, Attribute.ICE_RESISTANCE)) / 5;
+			if (damage > 0) {
+				DamageAPI.registerDamageCause(this.entity, DamageCause.MELTING,
+						entity, 1);
+				EntityDamageByEntityEvent event = new EntityDamageByEntityEvent(
+						this.entity, entity,
+						EntityDamageByEntityEvent.DamageCause.MELTING, damage);
+				Bukkit.getPluginManager().callEvent(event);
+				if (!event.isCancelled()) {
+					double health = entity.getHealth() - event.getDamage();
+					if (health < 0) {
+						health = 0;
+					}
+					entity.setHealth(health);
+				}
+			}
+		}
 
-        for (Attribute attribute : attributeValues.keySet()) {
-            //Calculate damage
-            damage = attributeValues.get(attribute);
-            if (attribute.hasResistance()) {
-                damage -= AttributeAPI.getValue(entity, attribute.getResistance());
-            }
-            //Roll to see if the effect occurs
-            if (damage > 0 && armorEffeciency < Math.random() && weaponEfficiency > Math.random()) {
-                int duration = (int) damage * 20;
-                if (attribute.hasPotionEffect()) {
-                    PotionEffect effect = attribute.getPotionEffect(duration, 1);
-                    entity.addPotionEffect(effect, true);
-                    DamageCause cause = getDamageCause(effect);
-                    if (cause != null) {
-                        DamageAPI.registerDamageCause(this.entity, cause, entity, duration);
-                    }
-                } else if (attribute == Attribute.FIRE_DAMAGE) {
-                    entity.setFireTicks(duration);
-                    DamageAPI.registerDamageCause(this.entity, DamageCause.FIRE_TICK, entity, duration);
-                }
-            }
-        }
-    }
+		for (Attribute attribute : attributeValues.keySet()) {
+			// Calculate damage
+			damage = attributeValues.get(attribute);
+			if (attribute.hasResistance()) {
+				damage -= AttributeAPI.getValue(entity,
+						attribute.getResistance());
+			}
+			// Roll to see if the effect occurs
+			if (damage > 0 && armorEffeciency < Math.random()
+					&& weaponEfficiency > Math.random()) {
+				int duration = (int) damage * 20;
+				if (attribute.hasPotionEffect()) {
+					PotionEffect effect = attribute
+							.getPotionEffect(duration, 1);
+					entity.addPotionEffect(effect, true);
+					DamageCause cause = getDamageCause(effect);
+					if (cause != null) {
+						DamageAPI.registerDamageCause(this.entity, cause,
+								entity, duration);
+					}
+				} else if (attribute == Attribute.FIRE_DAMAGE) {
+					entity.setFireTicks(duration);
+					DamageAPI.registerDamageCause(this.entity,
+							DamageCause.FIRE_TICK, entity, duration);
+				}
+			}
+		}
+	}
 
-    /**
-     * Returns the DamageCause related to the given PotionEffect
-     *
-     * @param effect The given PotionEffect
-     * @return The DamageCause which occurs
-     */
-    private static DamageCause getDamageCause(PotionEffect effect) {
-        if (effect.getType() == PotionEffectType.POISON) {
-            return DamageCause.POISON;
-        } else if (effect.getType() == PotionEffectType.WITHER) {
-            return DamageCause.WITHER;
-        } else {
-            return null;
-        }
-    }
+	/**
+	 * Returns the DamageCause related to the given PotionEffect
+	 * 
+	 * @param effect
+	 *            The given PotionEffect
+	 * @return The DamageCause which occurs
+	 */
+	private static DamageCause getDamageCause(PotionEffect effect) {
+		if (effect.getType() == PotionEffectType.POISON) {
+			return DamageCause.POISON;
+		} else if (effect.getType() == PotionEffectType.WITHER) {
+			return DamageCause.WITHER;
+		} else {
+			return null;
+		}
+	}
 }
